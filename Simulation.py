a20bbf5066f1c3421c4f918ff662a6f4a3ae1da9
Nicolás Nieto <nnieto@sinc.unl.edu.ar>
2021-09-01 13:20:26 -0300

    delete commented lines

--- Simulation.py
+++ Simulation.py
@@ -40,59 +40,14 @@ def simular_iteraciones_Ridge_plot(info, sr, situacion, best_alpha, iteraciones,
 
         # Error
         Rmse_fake = np.array(np.sqrt(np.power((predicho_fake - eeg_test),2).mean(0)))
-        
-        
-        # if not iteracion%100:
-        #     print('Plot')
-        #     plt.ion()
-        #     fig, ax = plt.subplots()
-        #     fig.suptitle('Sesion {} - Sujeto {} - Corr {:.2f}'.format(sesion, sujeto, np.mean(Rcorr_fake)))
-
-        #     evoked = mne.EvokedArray(mod_fake.coef_, info)
-        #     evoked.plot(scalings = dict(eeg=1, grad=1, mag=1), zorder = 'std', time_unit = 'ms', 
-        #                 show = False, spatial_colors=True, unit = False, units = 'w', 
-        #                 axes = ax)
-            
-        #     ax.plot(np.arange(0, mod_fake.coef_.shape[1]/sr*1000, 1000/sr), 
-        #                     mod_fake.coef_.mean(0),'k--', label = 'Mean', zorder = 130, linewidth = 2)
-            
-        #     ticks =  np.array(list(evoked.times[-1]*1000 - ax.get_xticks()[:-1]))
-        #     ax.set_xticks(list(ticks))
-        #     ax.set_xticklabels(list((ticks-evoked.times[-1]*1000).astype(int)))
-        #     # axs[0].set_xlabel('Time [ms]')
-        #     ax.xaxis.label.set_size(13)
-        #     ax.yaxis.label.set_size(13)
-        #     ax.legend(fontsize = 13)
-        #     ax.grid()
-        
-        # plt.ion()
-        # fig, axs = plt.subplots(2,1)
-        
-        # axs[0].plot(eeg_test[:,0], label = 'Señal')
-        # axs[0].plot(predicho_fake[:,0], label = 'Prediccion')
-        # axs[0].set_title('Señal original')
-        # axs[0].legend()
-        
-        # axs[1].plot(predicho_fake[:,0])
-        # axs[1].set_title('Señal predicha')
-        # axs[1].legend()
-        # fig.tight_layout()
-             
+           
         psds_test, freqs_mean = mne.time_frequency.psd_array_welch(eeg_test.transpose(), info['sfreq'], fmin, fmax)
+        
         psds_random, freqs_mean = mne.time_frequency.psd_array_welch(predicho_fake.transpose(), info['sfreq'], fmin, fmax)          
         
-        psds_channel_corr = np.array([np.corrcoef(psds_test[ii].ravel(), np.array(psds_random[ii]).ravel())[0,1] for ii in range(len(psds_test))])      
+        psds_channel_corr = np.array([np.corrcoef(psds_test[ii].ravel(), np.array(psds_random[ii]).ravel())[0,1] for ii in range(len(psds_test))])   
+        
         psds_rand_correlations.append(np.mean(psds_channel_corr))
-        
-        # fig, ax = plt.subplots()
-        # fig.suptitle('Sesion {} - Sujeto {} - Situacion {}'.format(sesion, sujeto, situacion))
-        
-        # evoked = mne.EvokedArray(psds_welch_mean, info)
-        # evoked.times = freqs_mean
-        # evoked.plot(scalings = dict(eeg=1, grad=1, mag=1), zorder = 'std', time_unit = 's', 
-        # show = False, spatial_colors=True, unit = False, units = 'w', axes = ax)
-        # ax.set_xlabel('Frequency [Hz]')
-        # ax.grid()
         
         print("\rProgress: {}%".format(int((iteracion+1)*100/iteraciones)), end='')
     return psds_rand_correlations
@@ -149,10 +104,7 @@ def simular_iteraciones_mtrf(iteraciones, sesion, sujeto, test_round, sr, info, 
         np.random.shuffle(dstims_test_random)    
         speech_test_random = dstims_test_random[:,0]
         speech_test_random = speech_test_random.reshape([speech_test_random.shape[0],1])
-        
-        # raw_train = mne.io.RawArray(eeg_train.transpose(), info)
-        # raw_test = mne.io.RawArray(eeg_test.transpose(), info)
-        
+       
         # Ajusto modelo Random
         rf_fake.fit(speech_train_random, eeg_train)
         # Predigo sobre estimulos random
