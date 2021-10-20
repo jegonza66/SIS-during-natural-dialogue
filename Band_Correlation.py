de66149a5e120b0fbb2084e05d7d60a07fece0e0
Joac <joaquin.gonzalez6693@gmail.com>
2021-10-20 15:44:04 -0300

    Band_search added

--- Band_Correlation.py
+++ Band_Correlation.py
@@ -7,8 +7,8 @@ import pickle
 Alphas = [10,100,500,1000]
 Alphas = [100]
 
-Bands_low = np.arange(0.1, 15, 0.2)
-Bands_range = np.flip(np.arange(0.1, 15, 0.2))
+Bands_low = np.arange(0.1, 10, 0.5)
+Bands_range = np.flip(np.arange(3, 7, 1))
 
 Correlations = np.zeros((len(Bands_low), len(Bands_range)))
 Errores = np.zeros((len(Bands_low), len(Bands_range)))
@@ -22,7 +22,7 @@ except:
 for alpha in Alphas:
     for i, l_freq in enumerate(Bands_low):
         print("\rProgress: {}%".format(int((i + 1) * 100 / len(Bands_low))), end='')
-        for j, h_freq in enumerate(Bands_low+Bands_range):
+        for j, h_freq in enumerate(l_freq+Bands_range):
             Correlations[i,j], Errores[i,j] = Run_function.run_pipeline(Band=(l_freq,h_freq), alpha=alpha)
 
 
@@ -34,10 +34,11 @@ for alpha in Alphas:
     fig = plt.figure()
     plt.title('Mean correlation values')
     plt.imshow(Correlations)
-    xticks = np.arange(Correlations.shape[0])
-    yticks = np.arange(Correlations.shape[1])
-    plt.xticks(xticks, labels=Bands_low)
-    plt.yticks(yticks, labels=np.sort((Bands_low + Bands_range))[::-1])
+    ax = plt.gca()
+    xticks = np.array(ax.get_xticks(), dtype=int)[1:-1]
+    yticks = np.array(ax.get_yticks(), dtype=int)[1:-1]
+    plt.xticks(xticks, labels=Bands_low[xticks].round(2))
+    plt.yticks(yticks, labels=Bands_range[yticks].round(2))
     plt.colorbar()
     plt.xlabel('Low frequency bandpass')
     plt.ylabel('Bandpass width in Hz')
