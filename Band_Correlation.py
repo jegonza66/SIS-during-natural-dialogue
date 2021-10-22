bbc5f3e55203fd3d488bbaf94a31119bf7114a31
Joac <joaquin.gonzalez6693@gmail.com>
2021-10-22 19:24:26 -0300

    Band_search added

--- Band_Correlation.py
+++ Band_Correlation.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 import os
 import pickle
 
-Alphas = [10,100,500,1000]
+Alphas = [10, 100, 500, 1000]
 Alphas = [100]
 
-Bands_low = np.arange(0.1, 15)
-Bands_range = np.flip(np.arange(3, 7, 1))
+Bands_low = np.flip(np.arange(0.1, 12))
+Bands_range = np.arange(1, 9, 0.5)
 
 Correlations = np.zeros((len(Bands_low), len(Bands_range)))
-Errores = np.zeros((len(Bands_low), len(Bands_range)))
 
 save_path = 'saves/Band_search/'
 try:
@@ -21,27 +20,30 @@ except:
 
 for alpha in Alphas:
     for i, l_freq in enumerate(Bands_low):
-        print("\rProgress: {}%".format(int((i + 1) * 100 / len(Bands_low))), end='')
-        for j, h_freq in enumerate(l_freq+Bands_range):
-            Correlations[i,j], Errores[i,j] = Run_function.run_pipeline(Band=(l_freq,h_freq), alpha=alpha)
+        for j, h_freq in enumerate(l_freq + Bands_range):
+            Correlations[i, j], _ = Run_function.run_pipeline(stim='Envelope', Band=(l_freq, h_freq), alpha=alpha)
 
+        print("\n\nProgress: {}%\n\n".format(int((i + 1) * 100 / len(Bands_low))))
 
-    f = open(save_path+'alpha_{}.pkl'.format(alpha), 'wb')
-    pickle.dump([Correlations, Errores], f)
+    f = open(save_path + 'alpha_{}.pkl'.format(alpha), 'wb')
+    pickle.dump(Correlations, f)
     f.close()
+
+    max_corr = np.argwhere(Correlations == Correlations.max())[0]
 
     plt.ion()
     fig = plt.figure()
     plt.title('Mean correlation values')
-    plt.imshow(Correlations)
+    im = plt.imshow(Correlations)
+    plt.text(max_corr[1], max_corr[0], Correlations.max().round(2), ha="center", va="center", color="red", size="small")
     ax = plt.gca()
-    xticks = np.array(ax.get_xticks(), dtype=int)[1:-1]
     yticks = np.array(ax.get_yticks(), dtype=int)[1:-1]
-    plt.xticks(xticks, labels=Bands_low[xticks].round(2))
-    plt.yticks(yticks, labels=Bands_range[yticks].round(2))
-    plt.colorbar()
-    plt.xlabel('Low frequency bandpass')
-    plt.ylabel('Bandpass width in Hz')
+    xticks = np.array(ax.get_xticks(), dtype=int)[1:-1]
+    plt.yticks(yticks, labels=Bands_low[yticks].round(2))
+    plt.xticks(xticks, labels=Bands_range[xticks].round(2))
+    plt.colorbar(shrink=0.95)
+    plt.ylabel('Low frequency bandpass')
+    plt.xlabel('Bandpass width in Hz')
     plt.tight_layout()
 
     fig.savefig('gráficos/Bands_Correlations_alpha{}.png'.format(alpha))
