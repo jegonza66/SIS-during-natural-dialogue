212dc6253177bffb1739980acf05c025a54002cd
Joac <joaquin.gonzalez6693@gmail.com>
2022-04-22 14:57:29 -0300

    Habla propia y silencio run con estadistica

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ tmin, tmax = -0.6, -0.003
 sr = 128
 delays = - np.arange(np.floor(tmin * sr), np.ceil(tmax * sr), dtype=int)
 times = np.linspace(delays[0] * np.sign(tmin) * 1 / sr, np.abs(delays[-1]) * np.sign(tmax) * 1 / sr, len(delays))
-situacion = 'Escucha'
+situacion = 'Habla_Propia'
 
 # Model parameters
 model = 'Ridge'
@@ -31,15 +31,15 @@ except:
     print('\n\nAlphas file not found.\n\n')
 
 # Stimuli and EEG
-Stims = ['Envelope', 'Pitch', 'Spectrogram']
-Bands = ['Delta', 'Theta', 'Alpha']
+Stims = ['Envelope']
+Bands = ['Theta']
 
 # Standarization
 Stims_preprocess = 'Normalize'
 EEG_preprocess = 'Standarize'
 
 # Random permutations
-Statistical_test = False
+Statistical_test = True
 
 # Save / Display Figures
 Display_Ind_Figures = False
